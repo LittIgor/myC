1b581de27deb99533875951efaedecdf38c099b1
Igor <stillfree007@mail.ru>
2012-03-23 14:34:19 +0200

    написа что то

---
+++ 1.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main()
+{
+  int a = 0;
+  int b = 0;
+  int c = 0;
+  int s = 0;
+  bool i = false;
+  /*  
+printf ("Vvedite 2 chisla\n");
+  do
+    {
+    scanf ("%d %d", &a &i);
+
+  }
+  while (p == a*i);
+  printf ("Teper' umnozhte ih=%d\n", p);
+ 
+  
+    {
+      printf ("Pravilno\n");
+    
+    }
+  */
+do
+    {
+  printf("try smt \n");
+  scanf ("%d %d %d", &a, &b, &s);
+  c = a*b;
+  if(c == s) i = true;
+  if(i == true){
+  printf("yes its right\n");
+  } else {
+  printf("not right\n");
+  }
+}
+while (1);
+}
